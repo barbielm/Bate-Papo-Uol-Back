f3cb787cafe387e1f968219d3d07d2aba1a2bdff
barbielm <barbosagabrie0508l@gmail.com>
2021-06-13 20:06:05 -0300

    remoção de participantes inativos

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import Joi from 'joi';
-import dayjs from 'dayjs';
-import 'dayjs/locale/pt-br.js';
+
 
 
 const port = 4000;
@@ -31,10 +30,11 @@ app.get('/participants', (req,res) => {
 app.post('/participants', (req,res) => {
     const participant = req.body;
     const {error, value} = schemaParticipant.validate(participant);
-    const now = dayjs().locale('pt-br').format('HH:mm:ss');
-    if(!error){
-        participants.push({...participant, lastStatus: now});
-        messages.push({from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: now})
+    const now = new Date();
+    const participantsNames = participants.map(p => p.name);
+    if(!error && !participantsNames.includes(participant.name)){
+        participants.push({...participant, lastStatus: now.toLocaleTimeString()});
+        messages.push({from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: now.toLocaleTimeString()})
         res.sendStatus(200);
     }
     else{
@@ -60,10 +60,10 @@ app.post('/messages', (req,res) => {
     const message = req.body;
     const from = req.headers.user;
     const {error, value} = schemaMessage.validate(message);
-    const now = dayjs().locale('pt-br').format('HH:mm:ss');
+    const now = new Date();
     const participantsNames = participants.map(p => p.name);
     if(!error && participantsNames.includes(from)){
-        messages.push({from: from , to: message.to, text: message.text, type: message.type, time: now});
+        messages.push({from: from , to: message.to, text: message.text, type: message.type, time: now.toLocaleTimeString()});
         res.sendStatus(200);
     }
     else{
@@ -72,7 +72,46 @@ app.post('/messages', (req,res) => {
 })
 
 app.post('/status', (req,res) => {
-    res.send('ok');
+    const user = req.headers.user;
+    const participantsNames = participants.map(p => p.name);
+    if(participantsNames.includes(user)){
+        for(let i = 0; i < participants.length; i++){
+            if(participants[i].name === user){
+                const now = new Date();
+                participants[i].lastStatus = now.toLocaleTimeString();
+            }
+        }
+    }
+    else {
+        res.sendStatus(400);
+    }
 })
+
+function removeParticipants(){
+    if(participants.length === 0){
+        return
+    }
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+    for(let i = 0; i < participants.length; i++){
+        let p = participants[i];
+        let lastSeen = parseInt(p.lastStatus[p.lastStatus.length - 2] + p.lastStatus[p.lastStatus.length - 1]);
+        lastSeen += parseInt(p.lastStatus[p.lastStatus.length - 5] + p.lastStatus[p.lastStatus.length - 4])*60;
+        lastSeen += parseInt(p.lastStatus[p.lastStatus.length - 8] + p.lastStatus[p.lastStatus.length - 7])*3600;
+        let seconds = parseInt(time[time.length - 2] + time[time.length - 1]);
+        seconds += parseInt(time[time.length - 5] + time[time.length - 4])*60;
+        seconds += parseInt(time[time.length - 8] + time[time.length - 7])*3600;
+        if(seconds - lastSeen > 10){
+            participants.splice(i,1);
+            messages.push({from: p.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: time});
+        }
+        else if(seconds - lastSeen < 0 && (24*60*60 + seconds - lastSeen) > 10){
+            participants.splice(i,1);
+            messages.push({from: p.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: time});
+        }
+        
+    }
+}
+setInterval(removeParticipants, 15000);
 
 app.listen(port, () => console.log("servidor rodando na porta " + port));
