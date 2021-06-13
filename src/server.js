cc94c2b9ef5e7f4febe78c3dbe107d19b8a9d31d
barbielm <barbosagabrie0508l@gmail.com>
2021-06-13 17:45:34 -0300

    get de mensagens

--- src/server.js
+++ src/server.js
@@ -7,12 +7,12 @@ import 'dayjs/locale/pt-br.js';
 
 const port = 4000;
 const schemaParticipant = Joi.object({
-    name: Joi.string().alphanum().min(1).required(),
+    name: Joi.string().min(1).required(),
 });
 
 const schemaMessage = Joi.object({
-    to: Joi.string().alphanum().min(1).required(),
-    text: Joi.string().alphanum().min(1).required(),
+    to: Joi.string().min(1).required(),
+    text: Joi.string().min(1).required(),
     type: Joi.string().valid('message', 'private_message').required(),
 }).with('to', 'text').with('text', 'type');
 
@@ -44,18 +44,26 @@ app.post('/participants', (req,res) => {
 })
 
 app.get('/messages', (req,res) => {
-    console.log(req.headers.user)
-    console.log(req.query.limit)
-    const userMessages = messages.filter(m => m)
+    const user = req.headers.user;
+    const limit = req.query.limit;
+    const userMessages = messages.filter(m => m.type === 'status' || m.type === 'message' || (m.type === 'private_message' && (m.from === user || m.to === user)))
+    if(!!limit && limit < userMessages.length){
+        const mostRecentMessages = userMessages.filter((m,i) => (i - limit) >= 0);
+        res.send(mostRecentMessages);
+    }
+    else {
+        res.send(userMessages);
+    }
 })
 
 app.post('/messages', (req,res) => {
     const message = req.body;
     const from = req.headers.user;
-    const {error, value} = schemaMessage.validate(participant);
+    const {error, value} = schemaMessage.validate(message);
     const now = dayjs().locale('pt-br').format('HH:mm:ss');
-    if(!error && participants.includes(from)){
-        messages.push({from: from , to: message.to, text: message.text, time: now});
+    const participantsNames = participants.map(p => p.name);
+    if(!error && participantsNames.includes(from)){
+        messages.push({from: from , to: message.to, text: message.text, type: message.type, time: now});
         res.sendStatus(200);
     }
     else{
